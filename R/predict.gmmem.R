ccc9769b13801495f2f7890453fcd275aad2b363
YSKoseki <ykoseki@otsuma.ac.jp>
2022-07-25 09:55:38 +0900

    Improve the drawing of mixture component PDFs

--- R/predict.gmmem.R
+++ R/predict.gmmem.R
@@ -1,7 +1,8 @@
-#' Calculate probability density functions of mixture components
+#' Extract predicted values from PDFs of mixture components
 #'
-#' `fitted.gmmem()` calculates probability density functions (PDFs) of
-#'     individual mixture components of the fitted Gaussian mixture model.
+#' `predict.gmmem()` extracts predicted values from probability density
+#'     functions (PDFs) of individual mixture components of the fitted Gaussian
+#'     mixture model.
 #'
 #' @importFrom dplyr `%>%` bind_cols rename_with
 #' @importFrom stringr str_replace
@@ -9,10 +10,12 @@
 #' @importFrom stats fitted sd dnorm
 #' @importFrom mixtools normalmixEM
 #' @param object A list object of class `gmmem`.
+#' @param newdata An optional vector of length n consisting of the new data. If
+#'     NULL (default), the fitted values are used.
 #' @param ... Unused.
-#' @return A data frame of class `fitted.gmmem` with three columns:
+#' @return A data frame of class `predict.gmmem` with three columns:
 #' \describe{
-#'   \item{x}{The raw data.}
+#'   \item{x}{The data to which probability densities are predicted.}
 #'   \item{comp}{Name of Mixture component.}
 #'   \item{density}{The probability densities.}
 #' }
@@ -22,18 +25,21 @@
 #' logmf <- log10(mifish)
 #' set.seed(101)
 #' mod <- gmmem(logmf, k = 3)
-#' pdf <- fitted(mod)
-#' @export fitted.gmmem
+#' pdf <- predict(mod)
+#' @export predict.gmmem
 #' @export
-fitted.gmmem <- function(object, ...) {
+predict.gmmem <- function(object, newdata = NULL, ...) {
   if (!inherits(object, "gmmem"))
     stop("use only with \'gmmem\' objects")
   . <- x <- NULL
+  if (is.null(newdata)) {
+    newdata <- object$x
+  }
   df <- 1:length(object$lambda) %>%
-    sapply(function(k) dgauss(object = object, k)) %>%
+    sapply(function(k) dgauss(object = object, newdata = newdata, k)) %>%
     bind_cols() %>%
     rename_with(~ str_replace(., "...", "Comp")) %>%
-    bind_cols(x = object$x, .) %>%
+    bind_cols(x = newdata, .) %>%
     pivot_longer(cols = -x, names_to = "comp", values_to = "density") %>%
     suppressMessages()
   class(df) <- c("data.frame")
@@ -48,11 +54,13 @@ fitted.gmmem <- function(object, ...) {
 #' @param comp A single number specifying the ID of mixture component.
 #' @noRd
 #
-dgauss <- function(object, k) {
-  x <- object$x
+dgauss <- function(object, newdata, k) {
+  if (is.null(newdata)) {
+    newdata <- object$x
+  }
   lambda <- object$lambda[k]
   mu <- object$mu[k]
   sigma <- object$sigma[k]
-  d <- lambda * dnorm(x, mean = mu, sd = sigma)
+  d <- lambda * dnorm(newdata, mean = mu, sd = sigma)
   return(d)
 }
